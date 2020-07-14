9f0ad0f9139681e2c3e20f67f5a53cf5911b9d37
shafou <jl_macedomatos@hotmail.com>
2020-07-14 16:31:48 -0400

    Starting vue router experiments

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from "vue";
 import store from "./store.js";
 import mixin from "./components/mixins.js";
 import App from "./App.vue";
+import lodash from "lodash";
+import VueRouter from "vue-router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+
+// router
+Vue.use(VueRouter);
+
+// lodash
+Vue.prototype.$_ = lodash;
 
 // bootstrap
 Vue.use(BootstrapVue);
