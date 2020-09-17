9e3f1dcd159e43c86630badb348efe9b8d44c537
shafou <jl_macedomatos@hotmail.com>
2020-09-17 13:46:36 -0400

    Deploying to github pages

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+
+set -e
+
+# build
+
+yarn run build
+
+# navigate into the build output directory
+
+cd dist
+
+# if you are deploying to a custom domain
+
+# echo 'www.example.com' > CNAME
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f git@github.com:shafouz/trello-clone.git master:gh-pages
+
+cd -
